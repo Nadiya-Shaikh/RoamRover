57029426c7b5e3b900dc018bec23e95cc3526633
Nadiya <killerbaytera@gmail.com>
2024-05-01 13:16:18 +0530

    phase_1a-created_route_for_listings

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const PORT = 8080;
-const MONGO_URL = "mongodb://127.0.0.1:27017/RoamRover";
+const MONGO_URL = "mongodb://127.0.0.1:27017/TheRoamRover";
 const Listing = require('./models/listing');
-
+const path = require('path');
 
 main()
     .then(() => console.log('mongodb connected successfully!'))
@@ -14,22 +14,35 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 };
 
-//check listing
-app.get('/testListing', async (req, res) => {
-    const sampleListing = new Listing({
-        title: 'Villa',
-        description: 'By the beach',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZTJ9PiXYRPBIWO2maHbR9UZHFId3Jj0aDTYR6uXROOA&s',
-        price: 2000,
-        location: 'Calangute, Goa',
-        country: 'India',
-    });
-    await sampleListing.save();
-    res.send('testing successful')
-    console.log('listing saved');
+// //check listing
+// app.get('/testListing', async (req, res) => {
+//     const sampleListing = new Listing({
+//         title: 'Villa',
+//         description: 'By the beach',
+//         image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZTJ9PiXYRPBIWO2maHbR9UZHFId3Jj0aDTYR6uXROOA&s',
+//         price: 2000,
+//         location: 'Calangute, Goa',
+//         country: 'India',
+//     });
+//     await sampleListing.save();
+//     res.send('testing successful')
+//     console.log('listing saved');
 
+// });
+
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+//index route
+app.get('/listings',async(req,res)=>{
+    try{
+        const allListings = await Listing.find({});
+        res.render("listings/index.ejs", {allListings});
+    }catch(err){
+        console.log(err);
+    }  
+  
 });
-
 app.get('/', (req, res) => {
     res.send('Home');
 });
--- init/index.js
+++ init/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const MONGO_URL = "mongodb://127.0.0.1:27017/RoamRover";
+const MONGO_URL = "mongodb://127.0.0.1:27017/TheRoamRover";
 const initData = require('./data');
 const Listing = require('../models/listing');
 
