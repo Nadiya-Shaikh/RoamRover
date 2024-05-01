7e58cfc12e8f9565f7a1d304e4149147d2e7dd20
Nadiya <killerbaytera@gmail.com>
2024-05-01 08:37:05 +0530

    phase_1a-listing_model

---
+++ models/listing.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: {
+        filename: String,
+        url: String,
+    },
+    price: {
+        type: String,
+        required: true,
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    country: {
+        type: String,
+        required: true,
+    }
+
+});
+
+const Listing = mongoose.model('Listing', listingSchema);
+module.exports = Listing;
