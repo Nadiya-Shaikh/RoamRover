69e91eee9cd34981d8e4f182aac8790225a5b200
Nadiya <killerbaytera@gmail.com>
2024-05-01 08:42:15 +0530

    phase_1a-initialize_database

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require('mongoose');
 const PORT = 8080;
 const MONGO_URL = "mongodb://127.0.0.1:27017/RoamRover";
+const Listing = require('./models/listing');
+
 
 main()
     .then(() => console.log('mongodb connected successfully!'))
@@ -10,7 +12,23 @@ main()
 
 async function main() {
     await mongoose.connect(MONGO_URL);
-}
+};
+
+//check listing
+app.get('/testListing', async (req, res) => {
+    const sampleListing = new Listing({
+        title: 'Villa',
+        description: 'By the beach',
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZTJ9PiXYRPBIWO2maHbR9UZHFId3Jj0aDTYR6uXROOA&s',
+        price: 2000,
+        location: 'Calangute, Goa',
+        country: 'India',
+    });
+    await sampleListing.save();
+    res.send('testing successful')
+    console.log('listing saved');
+
+});
 
 app.get('/', (req, res) => {
     res.send('Home');
---
+++ init/index.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const MONGO_URL = "mongodb://127.0.0.1:27017/RoamRover";
+const initData = require('./data');
+const Listing = require('../models/listing');
+
+main()
+    .then(() => console.log('mongodb connected successfully!'))
+    .catch((err) => console.log("ERORR - ", err));
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+};
+
+const initDB = async()=>{
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+    console.log('data saved successfully!');
+}
+
+initDB();
